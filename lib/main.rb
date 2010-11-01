536c43c8cb118307e1ae2401fdc5c946ee28e9db
Will Booth <will@purplebooth.co.uk>
2010-11-01 19:28:30 +0000

    Shuffled the Added and the current queue notification messages in order to make the added message pop-up before the status message

--- lib/main.rb
+++ lib/main.rb
@@ -6,18 +6,18 @@ require File.dirname(__FILE__) + '/sabnzbd_plus/api'
 CHECK_EVERY_SECONDS = 60*5
 ## Tick Tock
 while true
-  queue = SabnzbdPlus::Api.current_queue
-  
-  queue.slots.each { |slot|
-    announce = slot.name + " ["+slot.mb_left+"MB/" + slot.mb + "MB @ "+queue.kb_per_sec+"KB/S ("+slot.timeleft+" timeleft)]"
-    puts announce
-    Notify::LibNotify.new.send announce
-  }
-
   added = SabnzbdPlus::Api.unannounced_added
 
   added.each { |slot|
     announce = "Added " + slot.name
+    puts announce
+    Notify::LibNotify.new.send announce
+  }
+
+  queue = SabnzbdPlus::Api.current_queue
+  
+  queue.slots.each { |slot|
+    announce = slot.name + " ["+slot.mb_left+"MB/" + slot.mb + "MB @ "+queue.kb_per_sec+"KB/S ("+slot.timeleft+" timeleft)]"
     puts announce
     Notify::LibNotify.new.send announce
   }
