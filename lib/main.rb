a98a35813dc911425ddbaa9dac3fb12d51e65c54
Will Booth <will@purplebooth.co.uk>
2010-11-07 15:02:27 +0000

    Fixed actual program

--- lib/main.rb
+++ lib/main.rb
@@ -5,21 +5,24 @@ require 'notify/api'
 require 'sabnzbd_plus/api'
 
 CHECK_EVERY_SECONDS = 60*5
+
+@api = SabnzbdPlus::Api.new
+
 ## Tick Tock
 while true
-  added = SabnzbdPlus::Api.unannounced_added
+  added = @api.unannounced_added
 
   added.each { |slot|
     Notify::Api.new.added_nzb slot.name
   }
 
-  queue = SabnzbdPlus::Api.current_queue
+  queue = @api.current_queue
   
   queue.slots.each { |slot|
     Notify::Api.new.current_status(slot.name, slot.mb_left, slot.mb, queue.kb_per_sec, slot.timeleft)
   }
 
-  complete = SabnzbdPlus::Api.unannounced_complete
+  complete = @api.unannounced_complete
 
   complete.each { |slot|
     Notify::Api.new.completed_nzb(slot.name, slot.status)
